e0f347da765039b90a94d2e956d1f664b8574f6b
Anatoly Tuzovsky <taf@artcollege.ru>
2024-05-07 18:41:50 +0300

    Первый вариант

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Practica11new;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите X = ");
+        double x = Convert.ToDouble(Console.ReadLine());
+        double F= Math.Pow(x,2) + 5;
+        Console.WriteLine($"F = {F}.");
+        Console.ReadLine();
+    }
+}
